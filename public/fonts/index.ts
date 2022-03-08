99aea6d94c022f120a1828098dbcf255e85d64ad
Ema Lorenzo <aemalorenzo@gmail.com>
2022-03-08 13:28:10 -0300

    removed default font

---
+++ public/fonts/index.ts
@@ -0,0 +1,6 @@
+// place in this folder your custom fonts
+// if posible use Google Fonts or Typekit since Next.js already does Font Optimization
+// and adds them as inline font CSS at build time, so it's server side rendered
+// https://nextjs.org/docs/basic-features/font-optimization
+
+export {};
