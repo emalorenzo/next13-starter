d08ac7824322e6b83dc7b5e55dc64fe77a8a1109
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-01-25 17:45:38 -0300

    add app to tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/layouts/**/*.{js,ts,jsx,tsx}',
